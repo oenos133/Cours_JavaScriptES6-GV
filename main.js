3164771fff3251127d165d160c558d2056d3b31a
oenos <oenos@free.fr>
2021-01-01 14:43:50 +0100

    Les fonctions avec return

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 "use strict"
+function bravo(prenom, points){
+    console.log(`Bravo ${prenom}, vous avez gagné ${points} points`);
+    return Math.round(Math.random() * 100);
+
+}
+let bonus = bravo("Bob", 50);
+console.log(`Vous avez un bonus de ${bonus} points`);
