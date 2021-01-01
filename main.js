cbaa5dd3933e1993bf1cd04f2e425e33f2d86e5a
oenos <oenos@free.fr>
2021-01-01 15:55:10 +0100

    Utilisation de try et catch pour capturer des exceptions

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 "use strict"
-/*
-Factorielle :
-0! = 1
-1! = 1 * 0! = 1
-2! = 2 * 1! = 2
-3! = 3 * 2! = 6
-4! = 4 * 3! = 24
-n! = n * (n-1)!
-*/
-// Récursivité
-function facto(n) {
-	if (n == 0) {
-		return 1;
-	}
-	return n * facto(n - 1);
-}
-console.log(facto(10));
+try {
+    console.log(n);
 
+}catch (e) {
+    console.log(e.message);
+}
+console.log('OK');
+
+// limite de recursivité
+
+function f(){
+    k++
+    f();
+}
+let k = 0;
+
+try {
+    f();
+} catch{
+console.log(k);
+}
