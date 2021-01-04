8fe70ca1169b49bfd90b4c00c37aa1f232132a2f
oenos <oenos@free.fr>
2021-01-04 14:00:14 +0100

    Les prommesses avec une promise.race

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-let e = document.querySelector('.titre');
-console.log(e.textContent);
-console.log(e.innerHTML);
 
-let tab = document.querySelectorAll('.titre');
-for(e of tab){
-    console.log(e.textContent);
-}
+let p = new Promise((resolve, reject) => {
+    let test = Math.random() < .5;
+    console.log(test);
 
-console.log(out);
+ setTimeout(() => test ? resolve("OK"): reject("KO"), 2000);
+})
 
-console.log(out.querySelector('div'));
+.then(val => console.log( "SUCCES",val))
+.catch(val => console.log("ECHEC", val));
+
+//Afficher le timeout sous forme de promesse----
+
+let timeout = new Promise((resolve) => setTimeout(() => resolve("TIMEOUT"),5000) );
+
+Promise.race([p, timeout]).then(val => console.log(val == "TIMEOUT"? val: ''));
+
+//Afficher FIN---------------
+console.log("FIN");git 
