8d2bca872b7dea3b0bc45863c14de333141b3758
oenos <oenos@free.fr>
2021-01-04 14:16:40 +0100

    Le fetch sans les données

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-
-let p = new Promise((resolve, reject) => {
-    let test = Math.random() < .5;
-    console.log(test);
-
- setTimeout(() => test ? resolve("OK"): reject("KO"), 2000);
-})
-
-.then(val => console.log( "SUCCES",val))
-.catch(val => console.log("ECHEC", val));
-
-//Afficher le timeout sous forme de promesse----
-
-let timeout = new Promise((resolve) => setTimeout(() => resolve("TIMEOUT"),5000) );
-
-Promise.race([p, timeout]).then(val => console.log(val == "TIMEOUT"? val: ''));
-
-//Afficher FIN---------------
-console.log("FIN");git 
+let url = 'http://adok.org/form/films/films.php';
+//let url = 'http://httpstat.us/500';
+fetch(url)
+	.then(reponse => {
+		return reponse.ok ? reponse.json() : Promise.reject(Error(reponse.statusText));
+	})
+	.then(films => console.log(films))
+	.catch(erreur => console.log(`Erreur: ${erreur.message}`));
