7b8e72fce55a09c3df0d39310cc36fcae32880f5
oenos <oenos@free.fr>
2021-01-04 14:28:10 +0100

    Fetch avec des données

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let url = 'http://adok.org/form/films/films.php';
-//let url = 'http://httpstat.us/500';
-fetch(url)
+let fd = new FormData();
+
+fd.append('action', 'lister');  //clef, valeur
+let init = {
+    method: 'post',
+    body: fd
+}
+
+fetch(url, init)
 	.then(reponse => {
 		return reponse.ok ? reponse.json() : Promise.reject(Error(reponse.statusText));
 	})
-	.then(films => console.log(films))
+	.then(films => {
+        for(let film of films){
+            console.log(`"${film.titre}" de ${film.real}`)
+        }
+    })
 	.catch(erreur => console.log(`Erreur: ${erreur.message}`));
